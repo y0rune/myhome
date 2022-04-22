ae1d8fd399a8520e883acabddd073a9082e3a23a
Marcin Woźniak <y0rune@aol.com>
2022-04-22 12:37:17 +0200

    Added yaml-language-server, ansible-language-server

--- .config/nvim/installer.sh
+++ .config/nvim/installer.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-npm -g i pyright bash-language-server
+npm -g i pyright bash-language-server --force
+npm -g i yaml-language-server --force
+npm -g i @ansible/ansible-language-server --force
 GO111MODULE=on go install mvdan.cc/sh/v3/cmd/shfmt@latest
 pip install black
 pip3 install black
