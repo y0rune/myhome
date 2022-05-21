601648b7edfa23c5c0e42c65694865690d84073a
Marcin Woźniak <y0rune@aol.com>
2022-05-21 11:44:52 +0200

    Added new modules in installer

--- .config/nvim/installer.sh
+++ .config/nvim/installer.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-npm -g i pyright bash-language-server --force
+npm -g i pyright --force
+npm -g i bash-language-server --force
 npm -g i yaml-language-server --force
 npm -g i @ansible/ansible-language-server --force
 GO111MODULE=on go install mvdan.cc/sh/v3/cmd/shfmt@latest
+GO111MODULE=on go install golang.org/x/tools/gopls@latest
 pip install black
 pip3 install black
